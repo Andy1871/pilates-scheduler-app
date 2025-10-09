b7a566a8a6c9ff172d7acd7bd13fa2bf73e2431e
Andy <andyherbert1875@gmail.com>
2025-10-09 17:12:01 +0100

    removal of middleware

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-// middleware.ts
-export { auth as middleware } from "@/auth";
-
-// Run auth on everything EXCEPT these public paths
-export const config = {
-  matcher: [
-    "/((?!signin|api/_debug-db|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)",
-  ],
-};
